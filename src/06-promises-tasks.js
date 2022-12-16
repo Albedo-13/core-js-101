96e3bcd0dc33af742bd4f2b5fceb08837bf5e5a4
Pavel Prokopenya <pavel12minsk@mail.ru>
2022-12-16 14:51:07 +0300

    feat: partial implement 06-promises-tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,23 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  const willSheMarry = new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer !== 'boolean') {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+
+    if (isPositiveAnswer) {
+      resolve('Hooray!!! She said "Yes"!');
+    }
+    resolve('Oh no, she said "No".');
+  });
+
+  willSheMarry
+    .then((data) => data)
+    .catch((error) => error);
+
+  return willSheMarry;
 }
 
 
@@ -49,6 +64,7 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *
  */
 function processAllPromises(/* array */) {
+  // im too fucking stupid to do this task
   throw new Error('Not implemented');
 }
 
